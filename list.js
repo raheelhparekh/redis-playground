0b3df4b87a459d869f75cfd79ede87a8af008adb
raheelhparekh <rhparekh2003@gmail.com>
2025-07-21 20:56:26 +0530

    chore: update project structure and dependencies

--- list.js
+++
@@ -1,9 +0,0 @@
-const client= require ("./client.js")
-
-async function init() {
-  await client.lpush("lists", 2);
-  const result=await client.lpush("lists", 1);
-  console.log(result);
-}
-
-init()
