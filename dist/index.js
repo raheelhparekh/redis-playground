2a0489ce045fe60b64c541d6e2c225c89f9ee263
raheelhparekh <rhparekh2003@gmail.com>
2025-07-21 23:36:47 +0530

    feat: implement rate limiting and caching with Redis;

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,54 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const axios_1 = __importDefault(require("axios"));
+const ioredis_1 = __importDefault(require("ioredis"));
+const redis = new ioredis_1.default({ host: "localhost", port: Number(6379) });
 const app = (0, express_1.default)();
 const PORT = process.env.PORT || 8000;
 app.get("/", (req, res) => {
     return res.json({ status: "success" });
 });
-app.listen(PORT, () => console.log("Server is running at post", PORT));
+// rate limiting using redis
+app.use((req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    const key = "rate-limit";
+    const value = yield redis.get("rate-limit");
+    if (value === null) {
+        yield redis.set("rate-limit", 0);
+        yield redis.expire("rate-limit", 60); // after 60 seconds expire
+    }
+    if (Number(value) > 10) {
+        return res.status(429).json({ message: "Too many requests" });
+    }
+    yield redis.incr("rate-limit");
+    next();
+}));
+app.get("/books", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    // check cache in redis
+    const cachedValue = yield redis.get("totalPageCount");
+    if (cachedValue) {
+        {
+            console.log("Cache hitt");
+            return res.json({ totalPageCount: Number(cachedValue) });
+        }
+    }
+    const response = yield axios_1.default.get("https://api.freeapi.app/api/v1/public/books");
+    const total = response.data.data.data.reduce((acc, curr) => { var _a, _b; return !((_a = curr.volumeInfo) === null || _a === void 0 ? void 0 : _a.pageCount) ? 0 : ((_b = curr.volumeInfo) === null || _b === void 0 ? void 0 : _b.pageCount) + acc; }, 0);
+    // set cache
+    console.log("Cache miss");
+    yield redis.set("totalPageCount", total);
+    return res.json({ totalPageCount: total });
+}));
+app.listen(PORT, () => console.log("Server is running at port", PORT));
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
 import express from "express";
+import axios from "axios";
+import Redis from "ioredis";
+
+const redis = new Redis({ host: "localhost", port: Number(6379) });
 
 const app = express();
 
@@ -8,4 +12,47 @@ app.get("/", (req, res) => {
   return res.json({ status: "success" });
 });
 
-app.listen(PORT, () => console.log("Server is running at post", PORT));
+// rate limiting using redis
+app.use(async (req, res, next) => {
+  const key = "rate-limit";
+  const value = await redis.get("rate-limit");
+
+  if (value === null) {
+    await redis.set("rate-limit", 0);
+    await redis.expire("rate-limit", 60); // after 60 seconds expire
+  }
+  if (Number(value) > 10) {
+    return res.status(429).json({ message: "Too many requests" });
+  }
+
+  await redis.incr("rate-limit");
+  next();
+});
+
+app.get("/books", async (req, res) => {
+  // check cache in redis
+  const cachedValue = await redis.get("totalPageCount");
+  if (cachedValue) {
+    {
+      console.log("Cache hitt");
+      return res.json({ totalPageCount: Number(cachedValue) });
+    }
+  }
+
+  const response = await axios.get(
+    "https://api.freeapi.app/api/v1/public/books"
+  );
+  const total = response.data.data.data.reduce(
+    (acc: number, curr: { volumeInfo?: { pageCount?: number } }) =>
+      !curr.volumeInfo?.pageCount ? 0 : curr.volumeInfo?.pageCount + acc,
+    0
+  );
+
+  // set cache
+  console.log("Cache miss");
+  await redis.set("totalPageCount", total);
+
+  return res.json({ totalPageCount: total });
+});
+
+app.listen(PORT, () => console.log("Server is running at port", PORT));
